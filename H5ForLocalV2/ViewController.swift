5319bcb9acf3aaa6504227067b0553aa197f6ad8
fmz <fanmzdj@gmail.com>
2023-12-13 20:04:24 +0800

    Initial Commit

---
+++ H5ForLocalV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  H5ForLocalV2
+//
+//  Created by fmz on 2023/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ H5ForLocalV2UITests/H5ForLocalV2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  H5ForLocalV2UITestsLaunchTests.swift
+//  H5ForLocalV2UITests
+//
+//  Created by fmz on 2023/12/13.
+//
+
+import XCTest
+
+class H5ForLocalV2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
